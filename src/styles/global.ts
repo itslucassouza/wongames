a29bad1c9667505d5ca063ef3604986720eda073
Lucas Souza <lucasvini193@hotmail.com>
2022-03-23 22:34:08 -0300

    [add configs from storybook]

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,16 +1,33 @@
 /* eslint-disable prettier/prettier */
-import { createGlobalStyle, css } from 'styled-components'
+import {
+  createGlobalStyle,
+  css,
+  DefaultTheme,
+  GlobalStyleComponent
+} from 'styled-components'
 
-const GlobalStyles = createGlobalStyle`
+type GlobalStylesProps = {
+  removeBg?: boolean
+}
+
+const GlobalStyles: GlobalStyleComponent<
+  GlobalStylesProps,
+  DefaultTheme
+> = createGlobalStyle`
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+
+    &::before,
+      &::after {
+        box-sizing: inherit;
+      }
     }
 
-    ${({ theme }) => css`
+    ${({ theme, removeBg }) => css`
       html {
         font-size: 62.5%;
       }
@@ -18,6 +35,12 @@ const GlobalStyles = createGlobalStyle`
       body {
         font-family: ${theme.font.family};
         font-size: ${theme.font.sizes.medium};
+
+        ${!removeBg &&
+        css`
+          background-color: ${theme.colors.mainBg};
+        `}
+        background-color: ${theme.colors.mainBg};
       }
     `}
 
