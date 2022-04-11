96e27c64e165ed3c9d03b540ef09557d698e4eae
Lucas Souza <lucasvini193@hotmail.com>
2022-04-11 13:44:02 -0300

    [add test in Cart]

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,5 +1,22 @@
-import Cart from 'templates/Cart'
+import Cart, { CartProps } from 'templates/Cart'
 
-export default function CartPage() {
-  return <Cart />
+import itemsMock from 'components/CartList/mock'
+import gamesMock from 'components/GameCardSlider/mock'
+import higlightMock from 'components/Highlight/mock'
+import cardsMock from 'components/PaymentOptions/mock'
+
+export default function CartPage(props: CartProps) {
+  return <Cart {...props} />
+}
+
+export async function getServerSideProps() {
+  return {
+    props: {
+      items: itemsMock,
+      total: '$ 430,00',
+      cards: cardsMock,
+      recommendedGames: gamesMock,
+      recommendedHighlight: higlightMock
+    }
+  }
 }
